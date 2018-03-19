b15b4ecff705e757df61a25256915e0914dc004b
Peter Hart <12421031@students.lincoln.ac.uk>
2018-03-19 18:54:50 +0000

    testing

--- Artefact/Task 1/task1.m
+++ Artefact/Task 1/task1.m
@@ -7,10 +7,29 @@
 close all; clear; clc; % Reset environment
 
 currentDir = pwd;
-file1Dir = strcat(currentDir,'\dataset\Topimage0000.bmp');
-file2Dir = strcat(currentDir,'\dataset\Topimage0013.bmp');
+file1Dir = strcat(currentDir,'\dataset\TopImg0000.bmp');
+file2Dir = strcat(currentDir,'\dataset\TopImg0013.bmp');
 
 im1 = imread(file1Dir);
 im2 = imread(file2Dir);
 
+    
+max_luminosity = 100; 
+threshold = 254;
+
+srgb2lab = makecform('srgb2lab');
+lab2srgb = makecform('lab2srgb');
+im_lab = applycform(im1, srgb2lab);
+
+L = im_lab(:,:,1)/max_luminosity;
+
+im_adapthisteq = im_lab;
+im_adapthisteq(:,:,1) = adapthisteq(L)*max_luminosity;
+im_adapthisteq = applycform(im_adapthisteq,lab2srgb);
+
+im_greyscale = im_adapthisteq(:,:,1);
+im_bi = rgb2bi(im_greyscale,threshold);    
+im_bi = imfill(im_bi,'holes'); 
+
+imshow(im_bi);
 % end of script
